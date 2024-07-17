0d801e7eddb54de71ea52b67db3cf96705ba0e21
mahdi <mahdi.mohseni0000@gmail.com>
2024-07-18 02:09:32 +0330

    first commit

---
+++ six.js
@@ -0,0 +1,19 @@
+const modal = document.querySelector('.modal-container');
+const openModal =document.getElementById('openBtn');
+const closeModal =document.querySelector('.closeBtn');
+
+
+openModal.addEventListener('click', ()=>{
+    modal.style.display = 'block';
+})
+
+closeModal.addEventListener('click', ()=>{
+    modal.style.display = 'none';
+})
+
+window.addEventListener("click" , (event)=>{
+    if (event.target == modal){
+        modal.style.display = 'none';
+    }
+
+}) 
